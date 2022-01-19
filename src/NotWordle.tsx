8cc03aad25d71cb8fca638c334f411271c55e709
Juniper Overbeck <june@learnwithjuni.com>
2022-01-19 01:10:44 -0800

    finishing up some core pieces in the game reducer

--- src/NotWordle.tsx
+++ src/NotWordle.tsx
@@ -3,42 +3,111 @@ import { getWordleTester, PlaceResult } from './game'
 import Grid from '@mui/material/Grid'
 import Box from '@mui/material/Box'
 
-const words = new Set(['bear', 'leap', 'spler'])
+const words = new Set(['bear', 'leap', 'sper'])
 export enum LetterBoxState {
+  vacant,
   awaitingInput,
+  thirstyForInput,
   inputStandby,
   inputSending,
   incorrectLetter,
   misplacedLetter,
   correctLetter
 }
+// Throw this in the utils folder at some point?
+// sorry
+const getEntryProps2 = (positions: number) => (entry: string) =>
+  (entry.length < positions ? [...entry.split('')] : entry.split('')).map(
+    (letter: string) => ({
+      value: letter,
+      state: LetterBoxState.inputStandby
+    })
+  )
+const getEmptyLetterBoxRow = (letterCount: number) =>
+  new Array<LetterBoxProps>(letterCount).fill({
+    value: ' ',
+    state: LetterBoxState.vacant
+  })
+const getEmptyGridRows = (rows: number, letterCount: number) =>
+  new Array<LetterBoxProps[]>(rows).fill(getEmptyLetterBoxRow(letterCount))
+
+const placeResultToProps = (position: PlaceResult) => ({
+  value: position.letter,
+  state: position.isCorrect
+    ? LetterBoxState.correctLetter
+    : position.isInWord
+    ? LetterBoxState.misplacedLetter
+    : LetterBoxState.incorrectLetter
+})
+
+const getTopGrid =
+  (letterCount: number, attempts: number) =>
+  (
+    entryRow: LetterBoxProps[],
+    previousAttempts: PlaceResult[][]
+  ): LetterBoxProps[][] => {
+    return [
+      ...previousAttempts.map(attempt => attempt.map(placeResultToProps)),
+      entryRow
+      // remaining rows
+    ]
+  }
+
+const getEntryProps = (positions: number) => (entry: string) =>
+  [
+    ...entry.split('').map((letter: string) => ({
+      value: letter,
+      state: LetterBoxState.inputStandby
+    })),
+    ...(entry.length < positions
+      ? [
+          {
+            value: ' ',
+            state: LetterBoxState.thirstyForInput
+          }
+        ]
+      : []),
+    ...(entry.length < positions - 1
+      ? getEmptyLetterBoxRow(positions - entry.length - 1)
+      : [])
+  ]
 
 const stateMap: Record<LetterBoxState, string> = {
   [LetterBoxState.awaitingInput]: '#adc493',
+  [LetterBoxState.thirstyForInput]: '#adc493',
   [LetterBoxState.inputStandby]: '#adc493',
   [LetterBoxState.inputSending]: '#adc493',
   [LetterBoxState.incorrectLetter]: '#adc493',
   [LetterBoxState.misplacedLetter]: '#adc493',
-  [LetterBoxState.correctLetter]: '#adc493'
+  [LetterBoxState.correctLetter]: '#adc493',
+  [LetterBoxState.vacant]: '#adc493'
 }
+
 interface LetterBoxProps {
   value: string
   state: LetterBoxState
+  attempts?: number
 }
 
-const LetterBox: FC<LetterBoxProps> = ({ value, state }) => {
+const LetterBox: FC<LetterBoxProps> = ({ value, state, attempts }) => {
   return (
-    <Box
-      sx={{
-        p: 2,
-        // justifyContent: 'center',
-        bgcolor: stateMap[state],
-        fontSize: 72,
-        textAlign: 'center'
-      }}
+    <Grid
+      item
+      xs={1}
+      sx={{ ...(attempts ? { height: `${100 / attempts}%` } : {}) }}
     >
-      {value}
-    </Box>
+      <Box
+        sx={{
+          p: 2,
+          // justifyContent: 'center',
+          bgcolor: stateMap[state],
+          fontSize: 72,
+          textAlign: 'center'
+        }}
+      >
+        {`${value}`}
+      </Box>
+    </Grid>
   )
 }
 
@@ -46,79 +115,92 @@ interface AttemptState {
   entry: string
   previousAttempts: PlaceResult[][]
 }
+/*
+* get the empty rows i need
+      ...(getEmptyGridRows(
+        previousAttempts.length + 1 < attempts
+          ? attempts - previousAttempts.length - 1
+          : 0,
+        letterCount
+      ) || [])
+      */
 
 const WordleGrid: FC<{ letterCount: number; attempts: number }> = ({
   letterCount,
   attempts
 }) => {
-  // const [entry, setEntry] = useState('')
-  // const [warning, setShowWarning] = useState(true)
-  // const [previousAttempts, setPreviousAttempts] = useState([] as PlaceResult[][])
   const [gameState, setAttempt] = useReducer(
     (state: AttemptState, event: KeyboardEvent) => {
-      console.log(state)
-      const { entry } = state
       const wordleTester = getWordleTester(words, 'bear')
-      if (
+      const { entry, previousAttempts } = state
+      // get more words, randomize, blah blah blah
+      const currentAttempts =
+        event.key === 'Enter' && wordleTester.validator(entry)
+          ? [...previousAttempts, wordleTester.guessDelta(entry)]
+          : previousAttempts
+      const newEntry =
         'abcdefghijklmnopqrstuvwxyz'.includes(event.key) &&
         entry.length < letterCount
-      ) {
-        return {
-          entry: entry.concat(event.key),
-          previousAttempts: state.previousAttempts
-        }
+          ? entry.concat(event.key)
+          : event.key === 'Backspace' && entry.length > 0
+          ? entry.slice(0, entry.length - 1)
+          : event.key === 'Enter' && wordleTester.validator(entry)
+          ? ''
+          : entry
+      // Remove these from the game state reducer
+      //const newEntryRow = getEntryProps(letterCount)(newEntry)
+      //const grid = getGrid(letterCount, attempts)(newEntryRow, currentAttempts)
+      // Return rounds
+      return {
+        entry: newEntry,
+        previousAttempts: currentAttempts
       }
-      if (event.key === 'Enter') {
-        return {
-          entry: '',
-          previousAttempts: [
-            ...state.previousAttempts,
-            wordleTester.guessDelta(state.entry)
-          ]
-        }
-      }
-      if (event.key === 'Backspace' && entry.length > 0) {
-        return {
-          entry: entry.slice(0, entry.length - 1),
-          previousAttempts: state.previousAttempts
-        }
-      }
-      return state
     },
-    { entry: '', previousAttempts: [] }
+    {
+      entry: '',
+      previousAttempts: []
+    }
   )
   useEffect(() => {
     window.addEventListener('keyup', setAttempt)
-    console.log()
     return () => {
       window.removeEventListener('keyup', setAttempt)
     }
   }, [])
-  console.log('i was here')
-  // Get actual list of words in here at some point
-  // {warning && 'Bad input'}
+
   return (
-    <Box sx={{ display: 'flex', width: '100%', justifyContent: 'center' }}>
-      <Box sx={{ width: '60%' }}>
-        <Grid container columns={letterCount} spacing={1}>
-          {gameState.entry.split('').map((char, index) => (
-            <Grid item key={`${char}${index}`} xs={1}>
-              <LetterBox state={LetterBoxState.inputStandby} value={char} />
-            </Grid>
-          ))}
+    <Box
+      sx={{
+        display: 'flex',
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center'
+      }}
+    >
+      <Box sx={{ width: '60%', height: '80%' }}>
+        <Grid
+          container
+          sx={{ height: '100%' }}
+          columns={letterCount}
+          spacing={1}
+        >
+          {[
+            ...getTopGrid(letterCount, attempts)(
+              getEntryProps(letterCount)(gameState.entry),
+              gameState.previousAttempts
+            ),
+            ...getEmptyGridRows(attempts, letterCount)
+          ].map(row =>
+            row.map(({ value, state }: LetterBoxProps, index: number) => (
+              <LetterBox
+                key={`${value}${index}-inputrow`}
+                state={state}
+                value={value}
+                attempts={attempts}
+              />
+            ))
+          )}
         </Grid>
-        {gameState.previousAttempts.map((attempt, index) => (
-          <Grid container key={index} columns={letterCount} spacing={1}>
-            {attempt.map((letter: PlaceResult, innerIndex) => (
-              <Grid item key={innerIndex} xs={1}>
-                <LetterBox
-                  state={LetterBoxState.inputStandby}
-                  value={letter.letter}
-                />
-              </Grid>
-            ))}
-          </Grid>
-        ))}
       </Box>
     </Box>
   )
